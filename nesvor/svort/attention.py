d297c58d1a23731575c2df7de234a0dbd89b9929
daviddmc <junshen@mit.edu>
2022-12-27 10:13:53 -0500

    add SVoRT v2

--- nesvor/svort/attention.py
+++ nesvor/svort/attention.py
@@ -1,13 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Function
 import torchvision.models as tvm
 
 # modified from https://github.com/jadore801120/attention-is-all-you-need-pytorch
 class MultiHeadAttention(nn.Module):
     """Multi-Head Attention module"""
 
-    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.0):
+    def __init__(
+        self,
+        n_head,
+        d_model,
+        d_k,
+        d_v,
+        dropout=0.0,
+        activation="softmax",
+        prenorm=False,
+    ):
         super().__init__()
 
         self.n_head = n_head
@@ -20,14 +30,19 @@ class MultiHeadAttention(nn.Module):
         self.fc = nn.Linear(n_head * d_v, d_model, bias=True)
 
         self.attention = ScaledDotProductAttention(
-            temperature=d_k**0.5, dropout=dropout
+            temperature=d_k**0.5, dropout=dropout, activation=activation
         )
         self.dropout = nn.Dropout(dropout)
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        self.prenorm = prenorm
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
 
         x = x.unsqueeze(0)
+
+        residual = x
+        if self.prenorm:
+            x = self.layer_norm(x)
 
         q = x
         k = x
@@ -35,8 +50,6 @@ class MultiHeadAttention(nn.Module):
 
         d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
         sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
-
-        residual = q
 
         # Pass through the pre-attention projection: b x lq x (n*dv)
         # Separate different heads: b x lq x n x dv
@@ -50,16 +63,17 @@ class MultiHeadAttention(nn.Module):
         # if mask is not None:
         #    mask = mask.unsqueeze(1)   # For head axis broadcasting.
 
-        q, attn = self.attention(q, k, v)
+        q, attn = self.attention(q, k, v, mask)
 
         # Transpose to move the head dimension back: b x lq x n x dv
         # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
         q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
         q = self.fc(q)
         q = self.dropout(q)
-        q += residual
+        q = q + residual
 
-        q = self.layer_norm(q)
+        if not self.prenorm:  # post norm
+            q = self.layer_norm(q)
 
         q = q.squeeze(0)
 
@@ -69,17 +83,25 @@ class MultiHeadAttention(nn.Module):
 class ScaledDotProductAttention(nn.Module):
     """Scaled Dot-Product Attention"""
 
-    def __init__(self, temperature, dropout=0.0):
+    def __init__(self, temperature, dropout=0.0, activation="softmax"):
         super().__init__()
         self.temperature = temperature
         self.dropout = nn.Dropout(dropout)
+        if activation == "softmax":
+            self.activation = lambda x: F.softmax(x, dim=-1)
+        elif activation == "entmax":
+            self.activation = lambda x: entmax15(x, dim=-1)
+        else:
+            raise ValueError("Unknown activation!")
 
-    def forward(self, q, k, v):
+    def forward(self, q, k, v, neg_inf_mask=None):
         # b x n x lq x dv
         attn = torch.matmul(
             q / self.temperature, k.transpose(2, 3)
         )  # (b x n x lq x dv) (b x n x dv x lq)
-        attn = F.softmax(attn, dim=-1)
+        if neg_inf_mask is not None:
+            attn = attn + neg_inf_mask
+        attn = self.activation(attn)
         attn = self.dropout(attn)
         output = torch.matmul(attn, v)
 
@@ -89,36 +111,52 @@ class ScaledDotProductAttention(nn.Module):
 class PositionwiseFeedForward(nn.Module):
     """A two-feed-forward-layer module"""
 
-    def __init__(self, d_in, d_hid, dropout=0.0):
+    def __init__(self, d_in, d_hid, dropout=0.0, activation="relu", prenorm=False):
         super().__init__()
         self.w_1 = nn.Linear(d_in, d_hid)  # position-wise
         self.w_2 = nn.Linear(d_hid, d_in)  # position-wise
         self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
         self.dropout = nn.Dropout(dropout)
+        self.activation = getattr(F, activation)
+        self.prenorm = prenorm
 
     def forward(self, x):
-
         residual = x
-
-        x = self.w_2(F.relu(self.w_1(x)))
+        if self.prenorm:
+            x = self.layer_norm(x)
+        x = self.w_2(self.activation(self.w_1(x)))
         x = self.dropout(x)
-        x += residual
-
-        x = self.layer_norm(x)
-
+        x = x + residual
+        if not self.prenorm:  # post norm
+            x = self.layer_norm(x)
         return x
 
 
 class EncoderLayer(nn.Module):
     """Compose with two layers"""
 
-    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+    def __init__(
+        self,
+        d_model,
+        d_inner,
+        n_head,
+        d_k,
+        d_v,
+        dropout=0.1,
+        activation_attn="softmax",
+        activation_ff="relu",
+        prenorm=False,
+    ):
         super(EncoderLayer, self).__init__()
-        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout)
-        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout)
+        self.slf_attn = MultiHeadAttention(
+            n_head, d_model, d_k, d_v, dropout, activation_attn, prenorm
+        )
+        self.pos_ffn = PositionwiseFeedForward(
+            d_model, d_inner, dropout, activation_ff, prenorm
+        )
 
-    def forward(self, enc_input, slf_attn_mask=None):
-        enc_output, attn = self.slf_attn(enc_input)
+    def forward(self, enc_input, mask=None):
+        enc_output, attn = self.slf_attn(enc_input, mask)
         enc_output = self.pos_ffn(enc_output)
         return enc_output, attn
 
@@ -143,26 +181,53 @@ class PositionalEncoding(nn.Module):
 class TransformerEncoder(nn.Module):
     """A encoder model with self attention mechanism."""
 
-    def __init__(self, n_layers, n_head, d_k, d_v, d_model, d_inner, dropout):
+    def __init__(
+        self,
+        n_layers,
+        n_head,
+        d_k,
+        d_v,
+        d_model,
+        d_inner,
+        dropout,
+        activation_attn="softmax",
+        activation_ff="relu",
+        prenorm=False,
+    ):
 
         super().__init__()
 
         self.dropout = nn.Dropout(p=dropout)
         self.layer_stack = nn.ModuleList(
             [
-                EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
+                EncoderLayer(
+                    d_model,
+                    d_inner,
+                    n_head,
+                    d_k,
+                    d_v,
+                    dropout,
+                    activation_attn,
+                    activation_ff,
+                    prenorm,
+                )
                 for _ in range(n_layers)
             ]
         )
+        self.prenorm = prenorm
         self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
 
-    def forward(self, x, pos_enc):
+    def forward(self, x, pos_enc, mask=None):
 
         enc_output = self.dropout(x + pos_enc)
-        enc_output = self.layer_norm(enc_output)
+        if not self.prenorm:  # post-norm
+            enc_output = self.layer_norm(enc_output)
 
         for enc_layer in self.layer_stack:
-            enc_output, attn = enc_layer(enc_output)
+            enc_output, attn = enc_layer(enc_output, mask)
+
+        if self.prenorm:
+            enc_output = self.layer_norm(enc_output)  # pre-norm
 
         return enc_output, attn
 
@@ -172,7 +237,8 @@ class ResNet(nn.Module):
         super().__init__()
         resnet_fn = getattr(tvm, "resnet%d" % n_res)
         model = resnet_fn(
-            norm_layer=lambda x: nn.BatchNorm2d(x, track_running_stats=False)
+            pretrained=pretrained,
+            norm_layer=lambda x: nn.BatchNorm2d(x, track_running_stats=False),
         )
         model.fc = nn.Linear(model.fc.in_features, d_model)
         if not pretrained:
@@ -186,3 +252,315 @@ class ResNet(nn.Module):
         if self.pretrained:
             x = x.expand(-1, 3, -1, -1)
         return self.model(x)
+
+
+"""
+An implementation of entmax (Peters et al., 2019). See
+https://arxiv.org/pdf/1905.05702 for detailed description.
+
+This builds on previous work with sparsemax (Martins & Astudillo, 2016).
+See https://arxiv.org/pdf/1602.02068.
+"""
+
+# Author: Ben Peters
+# Author: Vlad Niculae <vlad@vene.ro>
+# License: MIT
+
+
+def _make_ix_like(X, dim):
+    d = X.size(dim)
+    rho = torch.arange(1, d + 1, device=X.device, dtype=X.dtype)
+    view = [1] * X.dim()
+    view[0] = -1
+    return rho.view(view).transpose(0, dim)
+
+
+def _roll_last(X, dim):
+    if dim == -1:
+        return X
+    elif dim < 0:
+        dim = X.dim() - dim
+
+    perm = [i for i in range(X.dim()) if i != dim] + [dim]
+    return X.permute(perm)
+
+
+def _sparsemax_threshold_and_support(X, dim=-1, k=None):
+    """Core computation for sparsemax: optimal threshold and support size.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        The input tensor to compute thresholds over.
+
+    dim : int
+        The dimension along which to apply sparsemax.
+
+    k : int or None
+        number of largest elements to partial-sort over. For optimal
+        performance, should be slightly bigger than the expected number of
+        nonzeros in the solution. If the solution is more than k-sparse,
+        this function is recursively called with a 2*k schedule.
+        If `None`, full sorting is performed from the beginning.
+
+    Returns
+    -------
+    tau : torch.Tensor like `X`, with all but the `dim` dimension intact
+        the threshold value for each vector
+    support_size : torch LongTensor, shape like `tau`
+        the number of nonzeros in each vector.
+    """
+
+    if k is None or k >= X.shape[dim]:  # do full sort
+        topk, _ = torch.sort(X, dim=dim, descending=True)
+    else:
+        topk, _ = torch.topk(X, k=k, dim=dim)
+
+    topk_cumsum = topk.cumsum(dim) - 1
+    rhos = _make_ix_like(topk, dim)
+    support = rhos * topk > topk_cumsum
+
+    support_size = support.sum(dim=dim).unsqueeze(dim)
+    tau = topk_cumsum.gather(dim, support_size - 1)
+    tau /= support_size.to(X.dtype)
+
+    if k is not None and k < X.shape[dim]:
+        unsolved = (support_size == k).squeeze(dim)
+
+        if torch.any(unsolved):
+            in_ = _roll_last(X, dim)[unsolved]
+            tau_, ss_ = _sparsemax_threshold_and_support(in_, dim=-1, k=2 * k)
+            _roll_last(tau, dim)[unsolved] = tau_
+            _roll_last(support_size, dim)[unsolved] = ss_
+
+    return tau, support_size
+
+
+def _entmax_threshold_and_support(X, dim=-1, k=None):
+    """Core computation for 1.5-entmax: optimal threshold and support size.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        The input tensor to compute thresholds over.
+
+    dim : int
+        The dimension along which to apply 1.5-entmax.
+
+    k : int or None
+        number of largest elements to partial-sort over. For optimal
+        performance, should be slightly bigger than the expected number of
+        nonzeros in the solution. If the solution is more than k-sparse,
+        this function is recursively called with a 2*k schedule.
+        If `None`, full sorting is performed from the beginning.
+
+    Returns
+    -------
+    tau : torch.Tensor like `X`, with all but the `dim` dimension intact
+        the threshold value for each vector
+    support_size : torch LongTensor, shape like `tau`
+        the number of nonzeros in each vector.
+    """
+
+    if k is None or k >= X.shape[dim]:  # do full sort
+        Xsrt, _ = torch.sort(X, dim=dim, descending=True)
+    else:
+        Xsrt, _ = torch.topk(X, k=k, dim=dim)
+
+    rho = _make_ix_like(Xsrt, dim)
+    mean = Xsrt.cumsum(dim) / rho
+    mean_sq = (Xsrt**2).cumsum(dim) / rho
+    ss = rho * (mean_sq - mean**2)
+    delta = (1 - ss) / rho
+
+    # NOTE this is not exactly the same as in reference algo
+    # Fortunately it seems the clamped values never wrongly
+    # get selected by tau <= sorted_z. Prove this!
+    delta_nz = torch.clamp(delta, 0)
+    tau = mean - torch.sqrt(delta_nz)
+
+    support_size = (tau <= Xsrt).sum(dim).unsqueeze(dim)
+    tau_star = tau.gather(dim, support_size - 1)
+
+    if k is not None and k < X.shape[dim]:
+        unsolved = (support_size == k).squeeze(dim)
+
+        if torch.any(unsolved):
+            X_ = _roll_last(X, dim)[unsolved]
+            tau_, ss_ = _entmax_threshold_and_support(X_, dim=-1, k=2 * k)
+            _roll_last(tau_star, dim)[unsolved] = tau_
+            _roll_last(support_size, dim)[unsolved] = ss_
+
+    return tau_star, support_size
+
+
+class SparsemaxFunction(Function):
+    @classmethod
+    def forward(cls, ctx, X, dim=-1, k=None):
+        ctx.dim = dim
+        max_val, _ = X.max(dim=dim, keepdim=True)
+        X = X - max_val  # same numerical stability trick as softmax
+        tau, supp_size = _sparsemax_threshold_and_support(X, dim=dim, k=k)
+        output = torch.clamp(X - tau, min=0)
+        ctx.save_for_backward(supp_size, output)
+        return output
+
+    @classmethod
+    def backward(cls, ctx, grad_output):
+        supp_size, output = ctx.saved_tensors
+        dim = ctx.dim
+        grad_input = grad_output.clone()
+        grad_input[output == 0] = 0
+
+        v_hat = grad_input.sum(dim=dim) / supp_size.to(output.dtype).squeeze(dim)
+        v_hat = v_hat.unsqueeze(dim)
+        grad_input = torch.where(output != 0, grad_input - v_hat, grad_input)
+        return grad_input, None, None
+
+
+class Entmax15Function(Function):
+    @classmethod
+    def forward(cls, ctx, X, dim=0, k=None):
+        ctx.dim = dim
+
+        max_val, _ = X.max(dim=dim, keepdim=True)
+        X = X - max_val  # same numerical stability trick as for softmax
+        X = X / 2  # divide by 2 to solve actual Entmax
+
+        tau_star, _ = _entmax_threshold_and_support(X, dim=dim, k=k)
+
+        Y = torch.clamp(X - tau_star, min=0) ** 2
+        ctx.save_for_backward(Y)
+        return Y
+
+    @classmethod
+    def backward(cls, ctx, dY):
+        (Y,) = ctx.saved_tensors
+        gppr = Y.sqrt()  # = 1 / g'' (Y)
+        dX = dY * gppr
+        q = dX.sum(ctx.dim) / gppr.sum(ctx.dim)
+        q = q.unsqueeze(ctx.dim)
+        dX -= q * gppr
+        return dX, None, None
+
+
+def sparsemax(X, dim=-1, k=None):
+    """sparsemax: normalizing sparse transform (a la softmax).
+
+    Solves the projection:
+
+        min_p ||x - p||_2   s.t.    p >= 0, sum(p) == 1.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        The input tensor.
+
+    dim : int
+        The dimension along which to apply sparsemax.
+
+    k : int or None
+        number of largest elements to partial-sort over. For optimal
+        performance, should be slightly bigger than the expected number of
+        nonzeros in the solution. If the solution is more than k-sparse,
+        this function is recursively called with a 2*k schedule.
+        If `None`, full sorting is performed from the beginning.
+
+    Returns
+    -------
+    P : torch tensor, same shape as X
+        The projection result, such that P.sum(dim=dim) == 1 elementwise.
+    """
+
+    return SparsemaxFunction.apply(X, dim, k)
+
+
+def entmax15(X, dim=-1, k=None):
+    """1.5-entmax: normalizing sparse transform (a la softmax).
+
+    Solves the optimization problem:
+
+        max_p <x, p> - H_1.5(p)    s.t.    p >= 0, sum(p) == 1.
+
+    where H_1.5(p) is the Tsallis alpha-entropy with alpha=1.5.
+
+    Parameters
+    ----------
+    X : torch.Tensor
+        The input tensor.
+
+    dim : int
+        The dimension along which to apply 1.5-entmax.
+
+    k : int or None
+        number of largest elements to partial-sort over. For optimal
+        performance, should be slightly bigger than the expected number of
+        nonzeros in the solution. If the solution is more than k-sparse,
+        this function is recursively called with a 2*k schedule.
+        If `None`, full sorting is performed from the beginning.
+
+    Returns
+    -------
+    P : torch tensor, same shape as X
+        The projection result, such that P.sum(dim=dim) == 1 elementwise.
+    """
+
+    return Entmax15Function.apply(X, dim, k)
+
+
+class Sparsemax(nn.Module):
+    def __init__(self, dim=-1, k=None):
+        """sparsemax: normalizing sparse transform (a la softmax).
+
+        Solves the projection:
+
+            min_p ||x - p||_2   s.t.    p >= 0, sum(p) == 1.
+
+        Parameters
+        ----------
+        dim : int
+            The dimension along which to apply sparsemax.
+
+        k : int or None
+            number of largest elements to partial-sort over. For optimal
+            performance, should be slightly bigger than the expected number of
+            nonzeros in the solution. If the solution is more than k-sparse,
+            this function is recursively called with a 2*k schedule.
+            If `None`, full sorting is performed from the beginning.
+        """
+        self.dim = dim
+        self.k = k
+        super(Sparsemax, self).__init__()
+
+    def forward(self, X):
+        return sparsemax(X, dim=self.dim, k=self.k)
+
+
+class Entmax15(nn.Module):
+    def __init__(self, dim=-1, k=None):
+        """1.5-entmax: normalizing sparse transform (a la softmax).
+
+        Solves the optimization problem:
+
+            max_p <x, p> - H_1.5(p)    s.t.    p >= 0, sum(p) == 1.
+
+        where H_1.5(p) is the Tsallis alpha-entropy with alpha=1.5.
+
+        Parameters
+        ----------
+        dim : int
+            The dimension along which to apply 1.5-entmax.
+
+        k : int or None
+            number of largest elements to partial-sort over. For optimal
+            performance, should be slightly bigger than the expected number of
+            nonzeros in the solution. If the solution is more than k-sparse,
+            this function is recursively called with a 2*k schedule.
+            If `None`, full sorting is performed from the beginning.
+        """
+        self.dim = dim
+        self.k = k
+        super(Entmax15, self).__init__()
+
+    def forward(self, X):
+        return entmax15(X, dim=self.dim, k=self.k)
--- nesvor/svort/models.py
+++ nesvor/svort/models.py
@@ -13,6 +13,8 @@ from ..transform import (
 from .attention import TransformerEncoder, PositionalEncoding, ResNet
 from ..slice_acquisition import slice_acquisition
 
+# main models
+
 
 class SVoRT(nn.Module):
     def __init__(self, n_iter=3, iqa=True, vol=True, pe=True, dropout=0.0):
@@ -114,6 +116,101 @@ class SVoRT(nn.Module):
         self.attn = attn.detach()
 
         return trans, volumes, thetas
+
+
+class SVoRTv2(nn.Module):
+    def __init__(self, n_iter=4, iqa=True, vol=True, pe=True):
+        super().__init__()
+        self.vol = vol
+        self.pe = pe
+        self.iqa = iqa and vol
+        self.attn = None
+        self.iqa_score = None
+        self.n_iter = n_iter
+
+        self.svrnet1 = SVRtransformerV2(
+            n_layers=4,
+            n_head=4 * 2,
+            d_in=9 + 2,
+            d_out=9,
+            d_model=256 * 2,
+            d_inner=512 * 2,
+            dropout=0.0,
+            n_channels=1,
+        )
+
+        self.svrnet2 = SVRtransformerV2(
+            n_layers=4 * 2,
+            n_head=4 * 2,
+            d_in=9 + 2,
+            d_out=9,
+            d_model=256 * 2,
+            d_inner=512 * 2,
+            dropout=0.0,
+            n_channels=2,
+        )
+
+        if iqa:
+            self.srr = SRR(n_iter=2, use_CG=True)
+
+    def forward(self, data):
+
+        params = {
+            "psf": data["psf_rec"],
+            "slice_shape": data["slice_shape"],
+            "interp_psf": False,
+            "res_s": data["resolution_slice"],
+            "res_r": data["resolution_recon"],
+            "s_thick": data["slice_thickness"],
+            "volume_shape": data["volume_shape"],
+        }
+
+        transforms = RigidTransform(data["transforms"])
+        stacks = data["stacks"]
+        positions = data["positions"]
+
+        thetas = []
+        volumes = []
+        trans = []
+
+        if not self.pe:
+            transforms = RigidTransform(transforms.axisangle() * 0)
+            positions = positions * 0 + data["slice_thickness"]
+
+        theta = mat2point(
+            transforms.matrix(), stacks.shape[-1], stacks.shape[-2], params["res_s"]
+        )
+        volume = None
+        mask_stacks = None
+
+        for i in range(self.n_iter):
+            svrnet = self.svrnet2 if i else self.svrnet1
+            theta, iqa_score, attn = svrnet(
+                theta,
+                stacks,
+                positions,
+                None if ((volume is None) or (not self.vol)) else volume.detach(),
+                params,
+            )
+            thetas.append(theta)
+            _trans = RigidTransform(point2mat(theta))
+            trans.append(_trans)
+            with torch.no_grad():
+                mat = mat_update_resolution(
+                    _trans.matrix().detach(), 1, params["res_r"]
+                )
+                volume = PSFreconstruction(mat, stacks, mask_stacks, None, params)
+            if self.iqa:
+                volume = self.srr(
+                    mat, stacks, volume, params, iqa_score.view(-1, 1, 1, 1)
+                )
+                self.iqa_score = iqa_score.detach()
+            volumes.append(volume)
+        self.attn = attn.detach()
+        return trans, volumes, thetas
+
+
+# transformers
 
 
 class SRRtransformer(nn.Module):
@@ -264,3 +361,97 @@ class SVRtransformer(nn.Module):
         x, attn = self.encoder(x, pe)
         x = self.fc(x)
         return theta + x * self.res_scale, attn
+
+
+class SVRtransformerV2(nn.Module):
+    def __init__(
+        self,
+        n_res=50,
+        n_layers=4,
+        n_head=4,
+        d_in=8,
+        d_out=6,
+        d_model=256,
+        d_inner=512,
+        dropout=0.1,
+        n_channels=2,
+    ):
+        super().__init__()
+        # self.img_encoder = ViT(channels=n_channels, num_classes=d_model)
+        self.img_encoder = ResNet(
+            n_res=n_res, d_model=d_model, pretrained=False, d_in=n_channels + 2
+        )
+        self.pos_emb = PositionalEncoding(d_model, d_in)
+        self.encoder = TransformerEncoder(
+            n_layers=n_layers,
+            n_head=n_head,
+            d_k=d_model // n_head,
+            d_v=d_model // n_head,
+            d_model=d_model,
+            d_inner=d_inner,
+            dropout=dropout,
+            activation_attn="softmax",
+            activation_ff="gelu",
+            prenorm=False,
+        )
+        self.fc = nn.Linear(d_model, d_out)
+        self.fc_score = nn.Linear(d_model, 1)
+
+    def pos_augment(self, slices, slices_est):
+        n, _, h, w = slices.shape
+        y = torch.linspace(-(h - 1) / 256, (h - 1) / 256, h, device=slices.device)
+        x = torch.linspace(-(w - 1) / 256, (w - 1) / 256, w, device=slices.device)
+        y, x = torch.meshgrid(y, x, indexing="ij")  # hxw
+        if slices_est is not None:
+            slices = torch.cat(
+                [
+                    slices,
+                    slices_est,
+                    y.view(1, 1, h, w).expand(n, -1, -1, -1),
+                    x.view(1, 1, h, w).expand(n, -1, -1, -1),
+                ],
+                1,
+            )
+        else:
+            slices = torch.cat(
+                [
+                    slices,
+                    y.view(1, 1, h, w).expand(n, -1, -1, -1),
+                    x.view(1, 1, h, w).expand(n, -1, -1, -1),
+                ],
+                1,
+            )
+        return slices
+
+    def forward(self, theta, slices, pos, volume, params, attn_mask=None):
+        y = volume
+        if y is not None:
+            with torch.no_grad():
+                transforms = mat_update_resolution(point2mat(theta), 1, params["res_r"])
+                y = slice_acquisition(
+                    transforms,
+                    y,
+                    None,
+                    None,
+                    params["psf"],
+                    params["slice_shape"],
+                    params["res_s"] / params["res_r"],
+                    False,
+                    params["interp_psf"],
+                )
+        pos = torch.cat((theta, pos), -1)
+        pe = self.pos_emb(pos)
+        if isinstance(self.img_encoder, ResNet):
+            slices = self.pos_augment(slices, y)
+        else:  # ViT
+            if y is not None:
+                slices = torch.cat([slices, y], 1)
+        x = self.img_encoder(slices)
+        x, attn = self.encoder(x, pe, attn_mask)
+        dtheta = self.fc(x)
+
+        score = self.fc_score(x)
+        score = F.softmax(score, dim=0) * score.shape[0]
+        score = torch.clamp(score, max=3.0)
+
+        return theta + dtheta, score, attn
